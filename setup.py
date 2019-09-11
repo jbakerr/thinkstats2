77754acd15a01eb2ca3979afa72962a3b7dc1925
Baker Renneckar <bakerrenneckar@gmail.com>
2019-09-11 15:53:35 -0700

    init commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setup
+
+setup(
+        name='thinkstats2',
+        version='0.1',
+        packages=setuptools.find_packages(),
+        install_requires=[
+            'scipy',
+            'pandas',
+            'numpy',
+            'thinkplot @ git+git://github.com/jbakerr/thinkplot'
+            ]
+        )
